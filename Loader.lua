cde469594141821582fba6e0c9f6cb9ba20dde82
Megzonshub <megzixhelopi@gmail.com>
2025-04-18 22:13:59 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,10 +1,8 @@
--- Megzons MPS Loader (ohne GUI, fix)
+-- Megzons MPS Loader ohne GUI (lädt Reach.lua direkt)
 local key = "Megzonshubmps"
 
 if _G.Key == key then
     print("Key korrekt! Lade Reach Script...")
-
-    -- Richtiger RAW-Link zu Reach.lua
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Megzonshub/MPS-Mobile/main/Reach.lua"))()
 else
     warn("Falscher Key! Zugriff verweigert.")
