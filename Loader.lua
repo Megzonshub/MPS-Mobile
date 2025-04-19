29d96f37f9e91de99eb3612afac666677daa1d2a
Megzonshub <megzixhelopi@gmail.com>
2025-04-19 22:05:38 +0200

    Loader lädt jetzt TestGUI.lua

--- Loader.lua
+++ Loader.lua
@@ -1,18 +1,1 @@
--- Megzons Mobile Hub - Loader
-local modules = {
-    "Reach.lua",
-    "Aimbot.lua",
-    "Powershot.lua",
-    "AntiKick.lua"
-}
-
-for _, module in ipairs(modules) do
-    local success, err = pcall(function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Megzonshub/MPS-Mobile/main/" .. module))()
-    end)
-    if success then
-        print(module .. " geladen.")
-    else
-        warn("Fehler beim Laden von " .. module .. ": " .. tostring(err))
-    end
-end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Megzonshub/MPS-Mobile/main/TestGUI.lua"))()
